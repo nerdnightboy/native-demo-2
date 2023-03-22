9380f47fea453b65f366f21dbf20c3cae7b6689b
nerdnightboy <nerdnightboy@gmail.com>
2023-03-22 20:05:10 +0530

    demo 2 complete

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator, ScrollView, Touchable, FlatList } from 'react-native';
 
-export default function App() {
+export default App = () =>  {
+
+  const {currdata, setdata} = useState([
+    {option:'Option A', key:'1'},
+    {option:'Option B', key:'2'},
+    {option:'Option C', key:'3'},
+    {option:'Option D', key:'4'},
+    {option:'Option E', key:'5'},
+    {option:'Option F', key:'6'},
+    {option:'Option G', key:'7'},
+    {option:'Option H', key:'8'},
+  ])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ScrollView style={mystyle.viewstyle}>
+        {currdata.map((item)=>(
+          <Touchable>
+          <Text key={item.key} style={mystyle.mytext}>
+            {item.option}
+          </Text>
+          </Touchable>
+        ))}
+      </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const mystyles = StyleSheet.create({
+
+  viewstyle:{
+    marginTop:40,
   },
+  mytext:{
+    color:"black",
+    fontSize:30,
+    backgroundColor:"yellow",
+    padding:10,
+    margin:10,
+    borderRadius:15,
+  },
+  // myimage:{
+  //   width:300,
+    // height:150,
+    // backgroundColor:"yellow",
+    // resizeMode:"contain",
+  // }
 });
